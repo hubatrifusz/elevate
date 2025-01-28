34b7011d6aeb439f805f82544d60ac1fe7394742
somloidavid <somloidavid@gmail.com>
2025-01-28 21:13:11 +0100

    Added required dependecies, Created DatabaseContext (Connection string requires further configuration)

---
+++ Backend/Elevate/Data/ElevateDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Elevate.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Elevate.Data
+{
+    public class ElevateDbContext(DbContextOptions<ElevateDbContext> options) : DbContext(options)
+    {
+        DbSet<UserModel> Users { get; set; }
+        DbSet<HabitModel> Habit { get; set; }
+        DbSet<AchievementModel> Achievements { get; set; }
+    }
+}
