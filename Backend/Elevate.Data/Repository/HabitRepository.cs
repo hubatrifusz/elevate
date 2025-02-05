e7e5db992f662dbadcde81b0eaa2212ead91e69f
somloidavid <somloi.david@students.jedlik.eu>
2025-02-05 10:30:02 +0100

    Removed obj folders

---
+++ Backend/Elevate.Data/Repository/HabitRepository.cs
@@ -0,0 +1,22 @@
+﻿using Elevate.Models.Habit;
+using Elevate.Models.User;
+
+namespace Elevate.Data.Repository
+{
+    public class HabitRepository(ElevateDbContext context)
+    {
+        readonly ElevateDbContext _context = context;
+
+        public HabitModel? GetHabitById(Guid habitId)
+        {
+            return _context.Set<HabitModel>().SingleOrDefault(h => h.Id == habitId);
+        }
+
+        public HabitModel? AddHabit(HabitModel habit)
+        {
+            UserModel savedHabit = _context.Set<HabitModel>().Add(habit).Entity;
+            _context.SaveChanges();
+            return savedHabit;
+        }
+    }
+}
