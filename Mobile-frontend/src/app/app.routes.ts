48af3dab92a09588bfa6becb6206ff7519b69767
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2025-02-16 21:20:48 +0100

    menu gombok keszek, valamit még szépiteni kell rajta, csinalni kell Collapse headert

--- Mobile-frontend/src/app/app.routes.ts
+++ Mobile-frontend/src/app/app.routes.ts
@@ -25,13 +25,8 @@ export const routes: Routes = [
     path: 'login-page',
     loadComponent: () => import('./pages/login-page/login-page.page').then(m => m.LoginPagePage)
   },
+  
 
-  // {
-  //   path: 'create-account-page',
-  //   loadComponent: () => import('./pages/create-account-page/create-account-page.page').then(m => m.CreateAccountPagePage)
-  // },
-  // {
-  //   path: 'feed',
-  //   loadComponent: () => import('./pages/feed/feed.page').then( m => m.FeedPage)
-  // }
+
+
 ];
--- Mobile-frontend/src/app/pages/feed/feed.page.ts
+++ Mobile-frontend/src/app/pages/feed/feed.page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { IonMenuToggle, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { Component, HostListener } from '@angular/core';
+import { IonMenuToggle, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonIcon, IonButton, IonCard, IonCardHeader, IonCardContent, IonItem, IonCardTitle, IonList, IonLabel, IonCheckbox } from '@ionic/angular/standalone';
 import { MenuController } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import { list, calendar, people, menu, settings, person, personCircle, personCircleOutline } from 'ionicons/icons';
+import { list, calendar, people, menu, settings, person, personCircle, personCircleOutline, personOutline, ribbon, ribbonOutline, logOutOutline } from 'ionicons/icons';
 import { FootertabsComponent } from "../../components/footertabs/footertabs.component";
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
@@ -11,17 +12,33 @@ import { FootertabsComponent } from "../../components/footertabs/footertabs.comp
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
   standalone: true,
-  imports: [IonMenuToggle, IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar]
+  imports: [IonCheckbox, IonLabel, IonList, IonCardTitle, IonItem, IonCardContent, IonCardHeader, IonCard, IonMenuToggle, IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, IonIcon]
 })
 export class FeedPage {
+  tasks: { title: string }[] = [];
 
 
-  constructor(private menuCtrl: MenuController) {
-    addIcons({personCircleOutline,personCircle,person,settings,people,menu});
+
+  constructor(private menuCtrl: MenuController, private router: Router) {
+    addIcons({ ribbonOutline, settings, logOutOutline, ribbon, personOutline, personCircleOutline, personCircle, person, people, menu });
+
+    this.generateTasks();
+  }
+  Logout() {
+    this.router.navigate(['/login-page']);
+  }
+
+
+  generateTasks() {
+    for (let i = 1; i <= 40; i++) {
+      this.tasks.push({ title: `Task ${i}` });
+    }
   }
 
   openMenu() {
     this.menuCtrl.toggle('');
   }
+   
+
 
 }
