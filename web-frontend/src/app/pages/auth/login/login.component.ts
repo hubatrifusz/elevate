ccac1f7b9d8a097e44313eecba0836461f2dfd12
huba.trifusz <huba.trifusz@gmail.com>
2025-02-18 11:20:38 +0100

    moved the Feature List to a component

--- web-frontend/src/app/pages/auth/login/login.component.ts
+++ web-frontend/src/app/pages/auth/login/login.component.ts
@@ -2,20 +2,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { inputValidator } from '../../../shared/input-validator';
+import { LoginFeatureListComponent } from "../../../components/auth/login-feature-list/login-feature-list.component";
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule, LoginFeatureListComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent {
-  ngOnInit() {
-    this.autoScrollFeatureCarousel();
-    this.addNavDots();
-  }
-
   togglePasswordView(): void {
     const icon = document.querySelector('#toggle_password_icon') as HTMLImageElement;
     const input = document.querySelector('#password_text_input') as HTMLInputElement;
@@ -29,49 +25,6 @@ export class LoginComponent {
       icon.src = 'icons/eye.png';
       icon.title = 'Show Password';
     }
-  }
-
-  autoScrollFeatureCarousel() {
-    const carouselDiv = document.querySelector('#features_container') as HTMLElement;
-    const navDots = document.querySelector('#nav_dots') as HTMLElement;
-    let index = 0;
-
-    const autoScroll = setInterval(() => {
-      carouselDiv.children[index + 1].scrollIntoView();
-
-      Array.from(navDots.children).forEach((element) => {
-        element.classList.remove('nav_dot_in_focus');
-      });
-
-      navDots.children[index].classList.add('nav_dot_in_focus');
-      index++;
-
-      setTimeout(() => {
-        if (index == carouselDiv.children.length - 1) {
-          index = 0;
-          carouselDiv.style.scrollBehavior = 'auto';
-          carouselDiv.scrollTo(0, 0);
-          carouselDiv.style.scrollBehavior = 'smooth';
-        }
-      }, 2000);
-    }, 5000);
-  }
-
-  addNavDots() {
-    const carouselDiv = document.querySelector('#features_container') as HTMLElement;
-    const carouselDivChildren = Array.from(carouselDiv.children);
-
-    const navDots = document.querySelector('#nav_dots') as HTMLElement;
-    navDots.innerHTML = '';
-
-    for (let i = 0; i < carouselDivChildren.length - 1; i++) {
-      const navDot = document.createElement('div');
-      navDot.classList.add('nav_dot');
-
-      navDots.appendChild(navDot);
-    }
-
-    navDots.children[navDots.children.length - 1].classList.add('nav_dot_in_focus');
   }
 
   loginForm = new FormGroup({
