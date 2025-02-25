0afb7205dee5217c7a80f9ba7d968a1210262878
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2025-02-25 13:19:19 +0100

    Header is working now, need a little bit of work. Fab icon left and woking search

--- Mobile-frontend/src/app/pages/feed/feed.page.ts
+++ Mobile-frontend/src/app/pages/feed/feed.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonMenuToggle, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonIcon, IonButton, IonCard, IonCardHeader, IonCardContent, IonItem, IonCardTitle, IonList, IonLabel, IonCheckbox, ScrollDetail, IonTabButton } from '@ionic/angular/standalone';
+import { IonMenuToggle, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonTitle, IonToolbar, IonIcon, IonButton, IonCard, IonCardHeader, IonCardContent, IonItem, IonCardTitle, IonList, IonLabel, IonCheckbox, ScrollDetail, IonTabButton, IonSearchbar } from '@ionic/angular/standalone';
 import { MenuController } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import { list, calendar, people, menu, settings, person, personCircle, personCircleOutline, personOutline, ribbon, ribbonOutline, logOutOutline, add, cogSharp } from 'ionicons/icons';
+import { list, calendar, people, menu, settings, person, personCircle, personCircleOutline, personOutline, ribbon, ribbonOutline, logOutOutline, add, cogSharp, menuOutline } from 'ionicons/icons';
 import { FootertabsComponent } from "../../components/footertabs/footertabs.component";
 import { Router, RouterLink } from '@angular/router';
 import { TaskCardComponent } from "../../components/task-card/task-card.component";
@@ -12,13 +12,17 @@ import { TaskCardComponent } from "../../components/task-card/task-card.componen
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
   standalone: true,
-  imports: [IonTabButton, IonCheckbox, IonLabel, IonList, IonCardTitle, IonItem, IonCardContent, IonCardHeader, IonCard, IonMenuToggle, IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, IonIcon, TaskCardComponent]
+  imports: [IonSearchbar, IonTabButton, IonCheckbox, IonLabel, IonList, IonCardTitle, IonItem, IonCardContent, IonCardHeader, IonCard, IonMenuToggle, IonButton, IonButtons, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, IonIcon, TaskCardComponent]
 })
 export class FeedPage {
   tasks: { title: string }[] = [];
+  private prevScrollPos: number = 0;
+  header = document.getElementsByTagName('ion-header');
+  searchbar = document.getElementsByTagName('ion-searchbar');
+  title = document.getElementsByTagName('ion-title');
 
   constructor(private menuCtrl: MenuController, private router: Router) {
-    addIcons({ ribbonOutline, settings, logOutOutline, add, ribbon, personOutline, personCircleOutline, personCircle, person, people, menu });
+    addIcons({ personCircleOutline, ribbonOutline, settings, logOutOutline, menuOutline, add, ribbon, personOutline, personCircle, person, people, menu });
 
   }
 
@@ -28,27 +32,43 @@ export class FeedPage {
     this.router.navigate(['/login-page']);
   }
 
-  header = document.getElementsByTagName('ion-header');
 
   handleScrollStart() {
     // console.log('scroll start');
   }
 
+  //Header scroll effect
   handleScroll(event: CustomEvent<ScrollDetail>) {
-    console.log('scroll', JSON.stringify(event.detail))
+    const currentScrollPos = event.detail.scrollTop;
+    console.log('scroll', JSON.stringify(event.detail));
     console.log(this.header);
-    if (this.header) {
-      if(event.detail.scrollTop > 30){
-        this.header[0].style.position = "fixed";
-        this.header[0].style.top = `-${event.detail.scrollTop}px`;
-      }
-      else if(event.detail.scrollTop < 30){
-        this.header[0].style.position = "static";
-      }
-    
-    }
-  }
 
+    if (this.header) {
+      if (currentScrollPos > this.prevScrollPos) {
+        // Scrolling down
+        if (currentScrollPos > 1) {
+          this.header[0].style.transition = "0.1s";
+          this.header[0].style.top = `-${currentScrollPos / 2}px`;
+          if (currentScrollPos > 130) {
+            this.title[0].style.display = "none";
+            this.searchbar[0].style.display = "block";
+          }
+        }
+      }
+      else {
+        // Scrolling up
+        this.header[0].style.transition = "0.1s";
+        this.header[0].style.top = "0px";
+      }
+      if (currentScrollPos < 2) {
+        this.header[0].style.transition = "0.1s";
+        this.title[0].style.display = "block";
+        this.searchbar[0].style.display = "none";
+      }
+    }
+
+    this.prevScrollPos = currentScrollPos;
+  }
   handleScrollEnd() {
     // console.log('scroll end');
   }
