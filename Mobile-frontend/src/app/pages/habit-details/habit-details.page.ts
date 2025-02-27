db44d84db9f74314161199ed27d1c649127e9c46
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2025-02-27 11:29:29 +0100

    fab button import, details back button

--- Mobile-frontend/src/app/pages/habit-details/habit-details.page.ts
+++ Mobile-frontend/src/app/pages/habit-details/habit-details.page.ts
@@ -1,20 +1,24 @@
 import { Component, Input, input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonMenuToggle, IonTitle, IonToolbar, IonTabButton, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonMenuToggle, IonTitle, IonToolbar, IonTabButton, IonButton, IonIcon, IonButtons, IonCardHeader, IonCard, IonCardSubtitle, IonCardTitle, IonCardContent, IonBackButton } from '@ionic/angular/standalone';
 import { Frequency, Habit } from 'src/app/.models/Habit.model';
 import { ActivatedRoute } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { caretBack, personCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-habit-details',
   templateUrl: './habit-details.page.html',
   styleUrls: ['./habit-details.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonIcon, IonButton, IonTabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuToggle]
+  imports: [IonBackButton, IonCardContent, IonCardTitle, IonCardSubtitle, IonCard, IonCardHeader, IonButtons, IonIcon, IonButton, IonTabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuToggle]
 })
 export class HabitDetailsPage implements OnInit {
   habit: Habit | undefined;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+    addIcons({ personCircleOutline, caretBack });
+  }
 
   ngOnInit() {
     const habitId = this.route.snapshot.paramMap.get('id');
