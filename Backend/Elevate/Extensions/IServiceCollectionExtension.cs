4b2b54b1f70e60ac9db1b6cd7baabd39913458d3
somloidavid <somloi.david@students.jedlik.eu>
2025-03-31 08:49:51 +0200

    Added log

--- Backend/Elevate/Extensions/IServiceCollectionExtension.cs
+++ Backend/Elevate/Extensions/IServiceCollectionExtension.cs
@@ -105,7 +105,7 @@ namespace Elevate.Extensions
         public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
             var publicKeyPem = configuration["Jwt:PublicKey"]
-                ?? throw new InvalidOperationException("Public key not configured.");
+                ?? throw new InvalidOperationException($"Public key not configured. {Jwt:PublicKey}");
 
             var rsa = RSA.Create();
             rsa.ImportRSAPublicKeyPem(publicKeyPem);
