af6edc01e167a29174fdead159498735227694c2
somloidavid <somloidavid@gmail.com>
2025-04-02 17:05:44 +0200

    Added new migration

---
+++ Backend/Elevate.Data/Migrations/20250402150527_RemovedChallengeUniqueConstraint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elevate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedChallengeUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Challenges_UserId_FriendId",
+                table: "Challenges");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_UserId_FriendId",
+                table: "Challenges",
+                columns: new[] { "UserId", "FriendId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Challenges_UserId_FriendId",
+                table: "Challenges");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_UserId_FriendId",
+                table: "Challenges",
+                columns: new[] { "UserId", "FriendId" },
+                unique: true);
+        }
+    }
+}
