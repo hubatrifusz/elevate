7b5168504dc49af59cac68c5acd063bcc3311fd8
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2025-04-15 15:49:02 +0200

    deleted google login button and doc update

--- Mobile-frontend/cypress/e2e/habit.cy.ts
+++ Mobile-frontend/cypress/e2e/habit.cy.ts
@@ -1,5 +1,5 @@
 describe('Habit tests', () => {
-  before(() => {
+  beforeEach(() => {
     cy.login("verba@gmail.com", "Asdf1234567(");
   })
   it('should create a new habit', () => {
@@ -8,20 +8,6 @@ describe('Habit tests', () => {
     cy.get('[data-cy=createHabit]').click();
     cy.url().should('include', '/footertabs/feed');
   })
-  it('should check the habit', () => {
-    cy.get('[data-cy=calendar-tab]').click();
-    cy.url().should('include', '/footertabs/calendar');
-    cy.get('ion-content:visible').should('exist');
-    cy.get('ion-content:visible [data-cy=habitCard]')
-      .first()
-      .should('be.visible')
-      .click();
-    cy.get('ion-content:visible [data-cy=checkHabit]')
-      .first()
-      .should('be.visible')
-      .click();
-    cy.get('.alert-button-role-confirm > .alert-button-inner').click()
-
-  });
+  
 
 })
