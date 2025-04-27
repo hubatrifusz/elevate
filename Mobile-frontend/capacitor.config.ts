9d2e142d023b6cf37b58e739f7efe30138412a08
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2025-04-27 19:20:24 +0200

    new icons

--- Mobile-frontend/capacitor.config.ts
+++ Mobile-frontend/capacitor.config.ts
@@ -10,6 +10,22 @@ const config: CapacitorConfig = {
     StatusBar: {
       overlaysWebView: false
     },
+    SpashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: '#7300FF',
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    }
   },
 };
 
